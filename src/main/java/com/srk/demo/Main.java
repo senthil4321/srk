14afd82fb6ef55618dfd9edbe3dbe3230fa8e2da
Senthil <senthil.jogi@gmail.com>
2020-05-23 07:34:09 +0200

    Update logger

--- src/main/java/com/srk/demo/Main.java
+++ src/main/java/com/srk/demo/Main.java
@@ -8,8 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
-        System.out.println("Hello World 2");
         logger.error("This is an error message");
     }
 
