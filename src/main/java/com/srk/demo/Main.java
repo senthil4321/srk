9a3e5ff05f633520dd03abf43b0d9675d1227a74
Senthil <senthil.jogi@gmail.com>
2020-05-24 10:11:37 +0200

    Update log in color

--- src/main/java/com/srk/demo/Main.java
+++ src/main/java/com/srk/demo/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
 
         logger.error("This is an error message");
+        logger.debug("Hello,{}", "Good Moring");
+        logger.info("@|KeyStyle {}|@ = @|ValueStyle {}|@", "one", "one");
     }
-
 }
