e4bece385a052146ef08792c772d2c75229f84ef
senthil <senthil22333@yahoo.co.in>
2021-02-27 21:42:26 +0100

    add jaxb marshal and unmarshal code

---
+++ src/main/java/com/srk/demo/jaxb/UnmarshalMain.java
@@ -0,0 +1,31 @@
+package com.srk.demo.jaxb;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class JaxbUnmarshalMain {
+
+    private static final Logger LOGGER = LogManager.getLogger(JaxbUnmarshalMain.class);
+
+    public static void main(String[] args) {
+
+	try {
+
+	    File file = new File("com.srk.demo.jaxb.name.input.xml");
+	    JAXBContext jaxbContext = JAXBContext.newInstance(NameModel.class);
+
+	    Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+	    NameModel nameModel = (NameModel) jaxbUnmarshaller.unmarshal(file);
+	    System.out.println(nameModel.getName());
+
+	} catch (JAXBException e) {
+	    e.printStackTrace();
+	}
+    }
+}
