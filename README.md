b7d1671cc33fb1061ee75439579f66f2c721a7ab
senthil <senthil22333@yahoo.co.in>
2021-02-27 16:11:26 +0100

    Add config provider

--- README.md
+++ README.md
@@ -4,7 +4,9 @@
 ## TODO
 1. Add sl4j
 1. Add mode appenders and logger example
+1. add handling to ignore config.property file not found error when there is no config file
 ## 
 1. https://travis-ci.com/senthil4321/srk.svg?branch=master
 ## done
 1. https://stackify.com/compare-java-logging-frameworks/
+1. add config provider
--- src/main/java/com/srk/demo/Main.java
+++ src/main/java/com/srk/demo/Main.java
@@ -1,15 +1,37 @@
 package com.srk.demo;
 
+import java.io.File;
+import java.util.Iterator;
+
+import org.apache.commons.configuration2.CompositeConfiguration;
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.SystemConfiguration;
+import org.apache.commons.configuration2.builder.fluent.Configurations;
+import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
     private static Logger logger = LogManager.getLogger(Main.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ConfigurationException {
 
-        logger.error("This is an error message");
-        logger.debug("Hello,{}", "Good Moring");
-        logger.info("@|KeyStyle {}|@ = @|ValueStyle {}|@", "one", "one");
+	CompositeConfiguration cconfig = new CompositeConfiguration();
+
+	Configurations configs = new Configurations();
+	Configuration config = configs.properties(new File("config.properties"));
+	
+	cconfig.addConfiguration(new SystemConfiguration());
+	cconfig.addConfiguration(config);
+
+        Iterator<String> keys = config.getKeys();
+        while (keys.hasNext()) {
+            String key = keys.next();
+            logger.info(key + " = " + config.getProperty(key));
+        }
+	
+	logger.error("This is an error message");
+	logger.debug("Hello,{}", "Good Moring");
+	logger.info("@|KeyStyle {}|@ = @|ValueStyle {}|@", "one", "one");
     }
 }
